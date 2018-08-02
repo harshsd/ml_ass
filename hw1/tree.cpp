a97016872e3e857270f513d0abfed58b05c908e3
harshsd <hdeshpande5998@gmail.com>
2018-08-03 01:55:05 +0530

    tree worked

--- hw1/tree.cpp
+++ hw1/tree.cpp
@@ -6,12 +6,13 @@
 #include <limits.h>
 #include <cmath>
 #include <math.h>
+#include <fstream>
 
 using namespace std;
 
 int column;
 
-float mean(vectore<float> &v,int start,int end)
+float mean(vector<float> &v,int start,int end)
 {
 	float mean = 0.0;
 	//cout<<"start and end"<<start<<" "<<end<<endl;
@@ -21,11 +22,11 @@ float mean(vectore<float> &v,int start,int end)
 
 float var(vector<float> &v,int start,int end)
 {
-	float mean = mean (v,start,end);
+	float mean_curr = mean (v,start,end);
 	//	cout <<"mean"<<mean<<endl;
-	float var = 0.0;
-	for(int k=start;k<end+1;k++) var += ((v[k]-mean)*(v[k]-mean))/(end-start+1);
-	return var;	
+	float var_curr = 0.0;
+	for(int k=start;k<end+1;k++) var_curr += ((v[k]-mean_curr)*(v[k]-mean_curr))/(end-start+1);
+	return var_curr;	
 
 }
 	
@@ -39,29 +40,45 @@ public:
 	float error_if_leaf;
 	float predicted_value;
 	int i_node,j_node;
+	vector<vector< float> > node_data;
 
-	Node(vector<vector<float>> &train)
+	Node(vector<vector<float> > &train)
 	{
+		int l1 = train.size();
+		int l2 = train[0].size();
+		//node_data = train;
 		L = NULL;
 		R = NULL;
 		is_leaf = true;
-		l1 = train.size;
-		l2 = train[0].size;
+		//cout<<"reached here";
+		int rows = train.size();
+		int cols = train[0].size();
+		for(int r=0;r<rows;r++)
+		{	vector<float> temp;
+			for(int c=0;c<cols;c++)
+			{
+				temp.push_back(train[r][c]);
+			}
+			node_data.push_back(temp);
+		}
+		// cout << l2;
 		vector<float> error_column_node ;/////////////////
 		for (int i=0;i<l1;i++)
 		{
-			error_column_node.push_back(l2-1][i])
+			error_column_node.push_back(train[i][l2-1]);
 		}
-		predicted_value = mean(error_column_node,0,l1);
-		error_if_leaf = sqrt(var(error_column_node,0,l1));
+		predicted_value = mean(error_column_node,0,l1-1);
+		//cout<<predicted_value;
+		error_if_leaf = sqrt(var(error_column_node,0,l1-1));
+		//cout<<"reached here";
 	}
 
-	bool sortcol(const vector<float>& v1, const vector<float>& v2) 
+	static bool sortcol(const vector<float>& v1, const vector<float>& v2) 
 	{
  		return v1[column] < v2[column];
 	}
 
-	float split(vector<vector<float> > &train,int row)
+	float split(vector<vector<float> > train,int row)
 	{
 	int l1 = train.size();
 	int l2 = train[0].size();
@@ -78,40 +95,58 @@ public:
 	return (val1*(row + 1) + val2*(l1 - row - 1))/l1;
 	}
 
-	float calc_best_fit(vector< vector<float> > &train)  //train[0] will contain first training example with n features and final value , thus lenght n+1
+	float calc_best_fit()  //train[0] will contain first training example with n features and final value , thus lenght n+1
 	{
-	int l1 = train.size();
-	int l2 = train[0].size();
+	int l1 = node_data.size();
+	int l2 = node_data[0].size();
 	int x,y;
 	float min_error = INT_MAX;
 
-	for(int i=0;i<l2;i++)
+	for(int i=0;i<l2-1;i++)
 	{
 		column = i;
-		sort(train.begin(),train.end(),sortcol);
-		for(int j=0;j<l1-1;j++)
+		sort(node_data.begin(),node_data.end(),sortcol);
+		for(int j=0;j<l1;j++)
 		{
 			//cout<<i<<endl;
-			float error = split(train,j);
+			float error = split(node_data,j);
 			if(min_error > error)
 			{
 				x = i;
-				y = train[j][i];
+				y = node_data[j][i];
 				min_error = error; 
 			}
 		}
 	}
 	i_node = x;
-	y_node = y;
+	j_node = y;
 	return min_error;
-	cout<<x<<" "<<y<<" "<<min_error<<endl;
+	//cout<<x<<" "<<y<<" "<<min_error<<endl;
 	}
 
-	void train_tree(vector<vector<float>> train_data)
-	{
-		float children_error = calc_best_fit(train_data);
+	void train_tree()
+	{	
+		vector<vector<float> > train_data;
+		//cout<<"chalu";
+		int rows = node_data.size();
+		if(rows == 1)
+		{
+			is_leaf = true;
+			return;
+		}
+		int cols = node_data[0].size();
+		for(int r=0;r<rows;r++)
+		{	vector<float> temp;
+			for(int c=0;c<cols;c++)
+			{
+				temp.push_back(node_data[r][c]);
+			}
+			train_data.push_back(temp);
+		}
+		float children_error = calc_best_fit();
 		if(children_error >= error_if_leaf)
 		{
+			//cout<<"shit";
 			is_leaf = true;
 			return;
 		}
@@ -120,13 +155,28 @@ public:
 			is_leaf = false;
 			column = i_node;
 			sort(train_data.begin(),train_data.end(),sortcol);
-			vector<vector<float> trainl,trainr;
-			for (int r=0;r<=y_node;r++) trainl.push_back(train_data[r]);
-			for (int r=y_node+1;r<train_data.size;r++) trainr.push_back(train_data[r]);	
+			vector<vector<float> > trainl;
+			vector<vector<float> > trainr;
+			//cout<<j_node;
+			//cout<<train_data.size();
+			for(int h=0;h<train_data.size();h++){
+				if(train_data[h][i_node]<=j_node) trainl.push_back(train_data[h]);
+				else trainr.push_back(train_data[h]);
+			}
+			int x1 = trainl.size();
+			int x2 = trainr.size();
+			if(x1==0 or x2==0) {is_leaf=true;return;}
 			L = new Node(trainl);
 			R = new Node(trainr);
-			L->train_tree(trainl);
-			R->train_tree(trainr);
+		// 	for(int j=0;j<trainl.size();j++)
+		// 		{for (int k=0;k<L->node_data[0].size();k++)cout<<L->node_data[j][k]<<" ";
+		// cout<<endl;}
+		// 	cout<<L->predicted_value<<endl;
+		// 	cout<<L->calc_best_fit()<<endl;
+		// 	cout<<L->j_node;
+		// 	cout<<L->i_node;
+			L->train_tree();
+			R->train_tree();
 			return;	
 		}
 	}
@@ -134,29 +184,39 @@ public:
 	float predict(vector<float> test)
 	{
 		if(is_leaf) return predicted_value;
-		else if (test[i_node]<=y) return L->predict;
-		else return R->predict;
+		else if (test[i_node]<=j_node) return L->predict(test);
+		else return R->predict(test);	
 	}
 
 };	
 
 
-int main()
-{
-	vector<float> v(5,0);
-	vector<vector<float> > a(5,v);
+// vector<vector<float> > readfile()
+// {	
+// }
 
-	for(int i=0;i<5;i++)
+int main()
+{	// readfile();
+	vector<float> v(3,0);
+	vector<vector<float> > a(100,v);
+
+	for(int i=0;i<100;i++)
 	{
-		for(int j=0;j<5;j++)
-		{
-			a[i][j] = rand()%5 + 1;
+		for(int j=0;j<3;j++)
+		{	if(j<2){
+			a[i][j] = rand()%4 + 1;}
+			else
+				a[i][j] = a[i][j-1]*a[i][j-2];
+
 			cout<<a[i][j]<<" ";
 		}
 		cout<<endl;
 	}
 	cout<<endl;
-	calc_best_fit(a);
-
+	Node root(a);
+	// cout<<mean(a[0],0,4)<<endl;
+	vector <float> l;l.push_back(2);l.push_back(3);
+	root.train_tree();
+	cout<<root.predict(l)<<endl;
 	return 0;
 }
